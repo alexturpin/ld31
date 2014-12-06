ebc6fbe48fdd7d7c5b202e002b80c2d7c05af94d
alexturpin <alex.turpin@gmail.com>
2014-12-06 11:18:45 -0500

    [+]Basic Socket.IO connectivity

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express'),
 	path = require('path');
 
-var app = express();
+var app = express(),
+	http = require('http').Server(app),
+	io = require('socket.io')(http);
 
 app.use('/', express.static(path.join(__dirname, 'client')));
+app.use('/shared', express.static(path.join(__dirname, 'shared')));
 
-app.listen(process.env.PORT || 80);
+var port = process.env.PORT || 80;
+http.listen(port, function() {
+	console.log("Listening on", port);
+
+	require('./game')(io);
+});
